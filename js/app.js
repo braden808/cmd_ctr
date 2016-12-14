4253b3b7f4e60bf00cc2e25879ab531eda7fd2ef
Braden Amorozo <braden808@gmail.com>
2016-12-14 09:17:03 -1000

    update

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function big() {
+	var changeDiv = document.getElementById("box1")
+	changeDiv.style.backgroundColor = "red";
+}
+
+function small(){
+	var changeDiv = document.getElementById("box1")
+	changeDiv.style.backgroundColor = "blue";
+}
